263e4acc05cf165f27e33ba7613c1a92dc0320de
ntunyuserge <ntunyuserge@gmail.com>
2023-04-29 13:30:26 +0100

    created components needed for the app

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
