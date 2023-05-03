1f3af11f7291a469efbd8a1d6a69c755c2369809
ntunyuserge <ntunyuserge@gmail.com>
2023-05-03 18:32:53 +0100

    Created input wrapper

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <>
+        <h2>NSN ~ Todo-App</h2>
+
+        <div className="input-wrapper">
+            <input type="text" placeholder="Create a new todo" />
+            <button>Add</button>
+        </div>
+
+
+
+
+      </>
+        
     </div>
   );
 }
