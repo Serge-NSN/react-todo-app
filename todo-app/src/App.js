e0cf66e6bae0a6fefe3ba0a871643f72ad90d88b
ntunyuserge <ntunyuserge@gmail.com>
2023-05-04 08:10:52 +0100

    completed todo app

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,38 +3,33 @@ import './App.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
-
-
-
 const App = () => {
 
 const [todos, setTodos] = useState([])
 const [todo, setTodo] = useState("")
-  
-
   const addTodo = () => {
+
     if(todo.length!==0){
     setTodos([...todos, todo]);
     setTodo("");
-    }  
-  }
+  
+  }}
 
   const removeItem = (text) => {
     const newTodos = todos.filter((todo)=>{ return todo !== text})
     setTodos(newTodos)
   }
 
-
   return (
     
     <div className="App">
-        <h2>NSN ~ Todo-App</h2>
+
+      <h1>Todo~App</h1>
         < TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo}/>
         < TodoList list={todos} removeItem={removeItem}/>
         
-        
     </div>
-  );
-}
+  
+);}
 
 export default App;
